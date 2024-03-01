a148124c3c2dd581ec7d6fe88a4a3ecf4654c57f
ilyavasilenko8 <qwegsg94@gmail.com>
2024-03-01 18:00:41 +0300

    test vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -166,3 +166,13 @@ void test_back_oneElementInVector(){
 
     assert(*res == 1);
 }
+
+void test(){
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+    test_popBack_notEmptyVector();
+    test_atVector_notEmptyVector();
+    test_atVector_requestToLastElement();
+    test_back_oneElementInVector();
+    test_front_oneElementInVector();
+}
--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include <stdint.h>
 #include "libs/data_structures/vector/vector.h"
 
-int main() {
-    vector v = createVector(0);
-
-    return 0;
+void test(){
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+    test_popBack_notEmptyVector();
+    test_atVector_notEmptyVector();
+    test_atVector_requestToLastElement();
+    test_back_oneElementInVector();
+    test_front_oneElementInVector();
 }
