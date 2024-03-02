d574d6f307075b7873a5921409865f0fe8d6f350
ilyavasilenko8 <qwegsg94@gmail.com>
2024-03-02 16:38:59 +0300

    refactoring / bug fix

--- main.c
+++ main.c
@@ -12,34 +12,11 @@ void test(){
     test_front_oneElementInVector();
 }
 
-void test1() {
-    size_t n;
-    scanf("%zd", &n);
-    vectorVoid v = createVectorV(0, sizeof(int));
-    for (int i = 0; i < n; i++) {
-        int x;
-        scanf("%d", &x);
-        pushBackV(&v, &x);
-    }
-    for (int i = 0; i < n; i++) {
-        int x;
-        getVectorValueV(&v, i, &x);
-        printf("%d ", x);
-    }
-}
+int main(){
+    test2();
 
-void test2() {
-    size_t n;
-    scanf("%zd", &n);
-    vectorVoid v = createVectorV(0, sizeof(float));
-    for (int i = 0; i < n; i++) {
-        float x;
-        scanf("%f", &x);
-        pushBackV(&v, &x);
-    }
-    for (int i = 0; i < n; i++) {
-        float x;
-        getVectorValueV(&v, i, &x);
-        printf("%f ", x);
-    }
+    //test1();
+    //test2();
+
+    return 0;
 }
